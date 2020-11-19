34abfa017c9f52d726447854252a0da167dbaed1
SKSteeve <steeve1997.ss@gmail.com>
2020-11-19 23:03:34 +0200

    Added messages for success and error

---
+++ OffTopic/resources/views/messages/success_and_error_messages.blade.php
@@ -0,0 +1,11 @@
+<div class="messages-create my-4 mx-4">
+
+    @if(session('success'))
+        <div class="alert alert-success">{{ session('success') }}</div>
+    @endif
+
+    @if(session('error'))
+        <div class="alert alert-danger">{{ session('error') }}</div>
+    @endif
+
+</div>
