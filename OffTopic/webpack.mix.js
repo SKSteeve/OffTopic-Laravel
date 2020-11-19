534f0e5f26e3255952d2bcf42ec3963334f1b9fe
SKSteeve <steeve1997.ss@gmail.com>
2020-11-19 23:05:33 +0200

    Add copy on jquery and fontawesome from node_modules to public folder

--- OffTopic/webpack.mix.js
+++ OffTopic/webpack.mix.js
@@ -11,5 +11,7 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix.js('resources/js/app.js', 'public/js/app.js')
+    .sass('resources/sass/app.scss', 'public/css/app.css')
+    .copy('node_modules/jquery/dist/jquery.js', 'public/js/jquery.js')
+    .copyDirectory('node_modules/@fortawesome/fontawesome-free', 'public/css/font-awesome');
