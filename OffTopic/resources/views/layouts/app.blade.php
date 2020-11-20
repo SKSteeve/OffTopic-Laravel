39da54209dc1fe583761f5abe7db5a8c60b36dcd
SKSteeve <steeve1997.ss@gmail.com>
2020-11-20 22:32:11 +0200

    Added message-remover.js that will remove every message on top after 5 seconds

--- OffTopic/resources/views/layouts/app.blade.php
+++ OffTopic/resources/views/layouts/app.blade.php
@@ -12,6 +12,7 @@
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
     <script src="{{asset('js/jquery.js')}}"></script>
+    <script src="{{ asset('js/messages-remover.js') }}"></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
