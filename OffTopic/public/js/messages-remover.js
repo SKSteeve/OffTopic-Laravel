7f030dee1e752127a44a42ed5ff92cf59b3c0483
SKSteeve <steeve1997.ss@gmail.com>
2020-11-20 22:32:43 +0200

    Created message-remover.js that will remove every message on top after 5 seconds

---
+++ OffTopic/public/js/messages-remover.js
@@ -0,0 +1,11 @@
+$('document').ready(function () {
+    //$('.messages-success-error').hide(8000);
+
+    setTimeout(function(){
+        let messageContainer = $('.messages-success-error');
+
+        if (messageContainer.length > 0) {
+            messageContainer.remove();
+        }
+    }, 5000)
+});
