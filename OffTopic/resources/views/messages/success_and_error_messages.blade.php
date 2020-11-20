c4bbe5a97285c3dac12a79319e51477d554269df
SKSteeve <steeve1997.ss@gmail.com>
2020-11-20 21:57:09 +0200

    Added validation error display

--- OffTopic/resources/views/messages/success_and_error_messages.blade.php
+++ OffTopic/resources/views/messages/success_and_error_messages.blade.php
@@ -1,4 +1,9 @@
 <div class="messages-create my-4 mx-4">
+    @if(count($errors) > 0)
+        @foreach($errors->all() as $error)
+             <div class="alert alert-danger">{{ $error }}</div>
+        @endforeach
+    @endif
 
     @if(session('success'))
         <div class="alert alert-success">{{ session('success') }}</div>
