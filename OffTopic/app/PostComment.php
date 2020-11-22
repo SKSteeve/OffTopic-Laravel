dd9292fcfd088e7ff1f72950b8a4a0d9ac672b5c
SKSteeve <steeve1997.ss@gmail.com>
2020-11-22 16:37:38 +0200

    Added Accessor (registred) for comment)

--- OffTopic/app/PostComment.php
+++ OffTopic/app/PostComment.php
@@ -9,6 +9,11 @@ class PostComment extends Model
     protected $table = 'posts_comments';
     protected $guarded = ['id'];
 
+    public function getRegisteredAttribute()
+    {
+        return $this->created_at->diffForHumans();
+    }
+
     public function post() {
         return $this->belongsTo('App\Post', 'post_id');
     }
