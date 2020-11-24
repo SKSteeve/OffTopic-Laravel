2680379733786dad95c5120799bafe3ab9872138
SKSteeve <steeve1997.ss@gmail.com>
2020-11-24 23:50:48 +0200

    Refactored some html and css classes names

--- OffTopic/resources/views/blog_views/post.blade.php
+++ OffTopic/resources/views/blog_views/post.blade.php
@@ -1,25 +1,24 @@
 @extends('layouts.app')
 
 @push('scripts')
-{{--    <script src="{{ asset('js/ajax-post-comments.js') }}"></script>--}}
+    <script src="{{ asset('js/ajax-post-comments.js') }}"></script>
 @endpush
 
 @section('content')
     <div class="px-5">
 
-        <div class="row m-0 mt-5 mb-0">
-            <h2 class="col-9 p-0">{{$post->title}}</h2>
+        <div class="m-0 mt-5 mb-0">
+            <h2 class="p-0 d-inline-block">{{$post->title}}</h2>
 
             @can('delete-post')
-                <button class="col-1 btn btn-danger mr-2 ml-5" onclick="document.getElementById('delete-form').submit();">Delete</button>
+                <button class="btn btn-danger float-right" onclick="document.getElementById('delete-form').submit();">Delete</button>
                 <form id="delete-form" action="{{ url('/blog',$post->id) }}/delete" method="POST" class="d-none">
                     @csrf
                     @method('DELETE')
-                    {{--  <input type="hidden" value="{{url('/')}}" id="url" name="url">--}}
                 </form>
             @endcan
             @can('edit-post')
-                <button class="col-1 btn btn-warning"><a class="text-decoration-none text-white" href="{{ url('/blog',$post->id) }}/edit">Edit</a></button>
+                <button class="btn btn-warning float-right mr-1"><a class="text-decoration-none text-white" href="{{ url('/blog',$post->id) }}/edit">Edit</a></button>
             @endcan
         </div>
         <small>published: {{$post->created_at}}</small>
@@ -32,7 +31,7 @@
 
         <div class="comments">
             @foreach($comments as $comment)
-                <div class="post-comment px-5">
+                <div class="post-comment px-5 pt-4 pb-2 mb-4 bg-light text-dark">
                     <h5 class="m-0 d-inline-block">{{ $comment->user->name }}</h5>
                     @auth
                         @if(Auth::user()->can('delete-edit-comments') || Auth::id() == $comment->user_id)
@@ -42,23 +41,24 @@
                                 <form id="delete-comment" action="{{ url('/blog', $post->id) }}/comment/{{ $comment->id }}/delete" method="POST" class="d-none">
                                     @csrf
                                     @method('DELETE')
-                                    {{--  <input type="hidden" value="{{url('/')}}" id="url" name="url">--}}
                                 </form>
                             </div>
 
                         @endif
                     @endauth
                     <br>
-                    <small>published: {{ $comment->created_at }}</small>
-                    <div class="post-comment-content my-4">
+                    <small>published: {{ $comment->registered }}</small>
+                    <div class="post-comment-content mt-3 pb-0">
                         <p>{{ $comment->body }}</p>
                     </div>
-                    <hr>
                 </div>
             @endforeach
 
             @auth
-                <button id="add-comment" class="btn btn-primary mx-5 float-right" type="button" data-toggle="collapse" data-target="#collapse-comment-form" aria-expanded="false"  aria-controls="collapse-comment-form">Add Comment</button><br/><br/>
+                <div class="create-editmode-comment-btn-section clearfix mx-5 mb-3">
+                    <button class="add-comment-btn btn btn-primary float-right" type="button" data-toggle="collapse" data-target="#collapse-comment-form" aria-expanded="false"  aria-controls="collapse-comment-form">Add Comment</button>
+                </div>
+                <div class="edit-comment-message"></div>
             @endauth
 
             <div class="collapse" id="collapse-comment-form">
@@ -67,10 +67,11 @@
                         @csrf
                         @method('POST')
 
-                        <label for="comment">Comment</label>
-                        <textarea name="body" class="comment" style="min-width: 100%" rows="5" ></textarea>
+                        <label for="comment" class="m-0">Comment</label>
+                        <div class="edit-mode-text text-success">You are about to create new comment!</div>
+                        <textarea name="body" class="comment-body-for-submit" style="min-width: 100%" rows="5" ></textarea>
                         <div class="d-flex justify-content-center">
-                            <button type="submit" class="btn btn-primary my-1 comment-form-submit-btn">Submit</button>
+                            <button type="submit" class="btn btn-primary my-1 comment-form-submit-btn">Post Comment</button>
                         </div>
                     </form>
                 </div>
