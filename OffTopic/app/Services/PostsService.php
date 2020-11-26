79dba018e656724fbc42f1a71ec5f6e032ac7cde
SKSteeve <steeve1997.ss@gmail.com>
2020-11-26 15:02:08 +0200

    Renamed validate func to validatePostFields and added new function validateCommentFields

--- OffTopic/app/Services/PostsService.php
+++ OffTopic/app/Services/PostsService.php
@@ -3,13 +3,30 @@
 
 namespace App\Services;
 use App\Post;
+use App\PostComment;
 use Illuminate\Support\Facades\Validator;
 
 class PostsService
 {
-    public static function validate($fields)
+    public static function validatePostFields($fields)
     {
         $validator = Validator::make($fields, Post::$rules, Post::$messages);
+
+        if($validator->fails()) {
+            return [
+                'status' => -1,
+                'errors' => $validator->errors()
+            ];
+        }
+
+        return $response = [
+            'status' => 1
+        ];
+    }
+
+    public static function validateCommentFields($fields)
+    {
+        $validator = Validator::make($fields, PostComment::$rules, PostComment::$messages);
 
         if($validator->fails()) {
             return [
