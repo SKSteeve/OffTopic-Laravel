9d9847174cd19a697e770797ff3ad4da433a7294
SKSteeve <steeve1997.ss@gmail.com>
2020-11-27 18:17:52 +0200

    Added some realistic content for comments and posts

--- OffTopic/database/seeds/PostsCommentsSeeder.php
+++ OffTopic/database/seeds/PostsCommentsSeeder.php
@@ -13,7 +13,7 @@ class PostsCommentsSeeder extends Seeder
     {
         DB::table('posts_comments')->insert([
             'id' => 1,
-            'body' => 'Komentar za tema s id 3',
+            'body' => 'I cant understand, can u help me please ?',
             'post_id' => 3,
             'user_id' => 2,
             'created_at' => now(),
@@ -23,7 +23,7 @@ class PostsCommentsSeeder extends Seeder
 
         DB::table('posts_comments')->insert([
             'id' => 2,
-            'body' => 'Komentar za tema s id 1',
+            'body' => 'Laravel suck dude, php suck, life suck HAHAAHA',
             'post_id' => 1,
             'user_id' => 3,
             'created_at' => now(),
@@ -33,7 +33,7 @@ class PostsCommentsSeeder extends Seeder
 
         DB::table('posts_comments')->insert([
             'id' => 3,
-            'body' => 'Komentar za tema s id 1',
+            'body' => 'I want to spam ...................',
             'post_id' => 1,
             'user_id' => 3,
             'created_at' => now(),
@@ -43,7 +43,7 @@ class PostsCommentsSeeder extends Seeder
 
         DB::table('posts_comments')->insert([
             'id' => 4,
-            'body' => 'Komentar za tema s id 1',
+            'body' => 'Nice. I like it too.',
             'post_id' => 1,
             'user_id' => 2,
             'created_at' => now(),
@@ -53,7 +53,7 @@ class PostsCommentsSeeder extends Seeder
 
         DB::table('posts_comments')->insert([
             'id' => 5,
-            'body' => 'Komentar za tema s id 5',
+            'body' => 'Im working with laravel for last 4 years and I agree with this.',
             'post_id' => 5,
             'user_id' => 5,
             'created_at' => now(),
@@ -62,7 +62,7 @@ class PostsCommentsSeeder extends Seeder
 
         DB::table('posts_comments')->insert([
             'id' => 6,
-            'body' => 'Komentar za tema s id 2',
+            'body' => 'By the end of 2017, 25 of 30 PHP developers in my technology firm switched from various other PHP frameworks: CodeIgniter, Yii, Zend, etc. to Laravel. This switch piqued my interest in Laravel and made me curious about this framework for php development. I decided to dig further only to find it isn’t just my firm everybody is making the switch.',
             'post_id' => 2,
             'user_id' => 5,
             'created_at' => now(),
@@ -71,9 +71,54 @@ class PostsCommentsSeeder extends Seeder
 
         DB::table('posts_comments')->insert([
             'id' => 7,
-            'body' => 'Komentar za tema s id 2',
+            'body' => 'My journey with Laravel started back in 2015. As a senior developer I can say that its a pleasure to work on Laravel and make different projects ... from small to medium. Its easy and fast to build professional apps. Your choice for framework is good.',
             'post_id' => 2,
+            'user_id' => 3,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        DB::table('posts_comments')->insert([
+            'id' => 8,
+            'body' => 'Nice project. I like it, but u still have a lot of work to do.',
+            'post_id' => 1,
             'user_id' => 5,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        DB::table('posts_comments')->insert([
+            'id' => 9,
+            'body' => 'I like your project. Hope you get hired as soon as you complete it :)',
+            'post_id' => 1,
+            'user_id' => 4,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        DB::table('posts_comments')->insert([
+            'id' => 10,
+            'body' => 'Yes, Laravel is probably the best php framework in 2020.',
+            'post_id' => 2,
+            'user_id' => 1,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        DB::table('posts_comments')->insert([
+            'id' => 11,
+            'body' => 'I like it too.',
+            'post_id' => 2,
+            'user_id' => 2,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        DB::table('posts_comments')->insert([
+            'id' => 12,
+            'body' => 'Its noobie project but keep going. One day you will become pro, just dont give up!',
+            'post_id' => 1,
+            'user_id' => 2,
             'created_at' => now(),
             'updated_at' => now(),
         ]);
--- OffTopic/database/seeds/PostsSeeder.php
+++ OffTopic/database/seeds/PostsSeeder.php
@@ -14,7 +14,7 @@ class PostsSeeder extends Seeder
         DB::table('posts')->insert([
             'id' => 1,
             'title' => 'My project',
-            'body' => 'This is the body, text of the post. Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage.',
+            'body' => 'Hello. Welcome to my project build to learn new technologies and get some experience. I used Laravel PHP framework, JS, jQuery, MySql, Bootstrap, HTML, CSS. The project is still in progress. I hope you like what you see for now.',
             'user_id' => 2,
             'created_at' => now(),
             'updated_at' => now(),
@@ -23,7 +23,7 @@ class PostsSeeder extends Seeder
         DB::table('posts')->insert([
             'id' => 2,
             'title' => 'I like Laravel',
-            'body' => 'This is the body, text of the post. Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage.',
+            'body' => 'Laravel has exploded in popularity. It has become one of the most popular, and widely used PHP frameworks in a short span of time. It\'s been close to 2 years since I have started using it; and I must confess, Laravel is indeed an elegant framework. This post will cover my experience using the Laravel framework, why I love it and why it is highly recommended for all the PHP developers. Laravel is an open source, modern PHP framework for building web applications rapidly. It also follows the Model-View-Controller (MVC) architectural pattern. Laravel ships with lots of great features which make building web applications simpler, expressive, easier, and faster. Some of these features which Laravel provides out of the box.',
             'user_id' => 3,
             'created_at' => now(),
             'updated_at' => now(),
@@ -32,7 +32,7 @@ class PostsSeeder extends Seeder
         DB::table('posts')->insert([
             'id' => 3,
             'title' => 'How to use Gates in Laravel',
-            'body' => 'This is the body, text of the post. Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage.',
+            'body' => 'Gates allow you to define an authorization rule using a simple closure-based approach. In other words, when you want to authorize an action that\'s not related to any specific model, the gate is the perfect place to implement that logic.',
             'user_id' => 5,
             'created_at' => now(),
             'updated_at' => now(),
@@ -41,7 +41,14 @@ class PostsSeeder extends Seeder
         DB::table('posts')->insert([
             'id' => 4,
             'title' => 'Abut Bulgarian nature',
-            'body' => 'This is the body, text of the post. Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage.',
+            'body' => '
+                Before trying to astonish you, I need to tell you some facts about Bulgaria’s geography first:
+
+                1. Tucked in the heart of the Balkan Peninsula in the South-Eastern part of Europe, at a crossroad between Europe and Asia, Bulgaria borders Turkey, Greece, Serbia, Macedonia, and Romania. Its natural borders are also the Black Sea and the Danube River.
+                2. With a size of almost 111,000 km2 (~43,000 mi2), Bulgaria is as big as Ohio, slightly smaller than neighbouring Greece, somewhere between the sizes of North Korea and South Korea, and approximately half the size of Uganda.
+                3. The highest Bulgarian mountain, Rila Mountain, is also the highest on the Balkans. Its tallest peak – Musala (2,925 m/9,596 ft.) – is higher than Mount Olympus by seven metres.
+                4. The mountain range, which gave the Balkan Peninsula its name, runs through the whole width of Bulgaria. The former name of what we now call Stara planina (literally: Old Mountain) was Balkan. It stretches from Serbia, divides Bulgaria into Northern and Southern, and kisses the Black Sea at Cape Emine.
+            ',
             'user_id' => 3,
             'created_at' => now(),
             'updated_at' => now(),
@@ -50,7 +57,18 @@ class PostsSeeder extends Seeder
         DB::table('posts')->insert([
             'id' => 5,
             'title' => 'Security in Laravel',
-            'body' => 'This is the body, text of the post. Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage.',
+            'body' => 'In my opinion, what you should be aware of:
+
+                1- It is not only Laravel alone that is responsible for your web application security, but the environment surrounding it.
+                    - Web server should be configured correctly and secure.
+                    - It is an advantage to SSL (Certificate) among your domain.
+                    - Do only use SFTP over SSH for file transfer and do only use SSH for console connection.
+                    - Use trusted provider and physically secured Server environment.
+                    - Backup your files and your database regularly and move the data out side your provider server location.
+                    - Make different username and password for SSH console, database or other services.
+                    - For SSH access and Database access, do not use admin or root username often, keep it only for emergency use, in stead create a sub admin/root account and use that in stead.
+                
+                2- Above all of that, when you further develop on your Laravel, you might risk performing bad programming which breaks the default security rules of Laravel.',
             'user_id' => 3,
             'created_at' => now(),
             'updated_at' => now(),
