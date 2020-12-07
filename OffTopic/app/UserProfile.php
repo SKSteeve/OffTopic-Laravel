133f869ad4ad4c8113ffdd43df3e754ef9a506a0
SKSteeve <steeve1997.ss@gmail.com>
2020-12-08 00:36:12 +0200

    Added validation rules and messages

--- OffTopic/app/UserProfile.php
+++ OffTopic/app/UserProfile.php
@@ -9,6 +9,21 @@ class UserProfile extends Model
     protected $table = 'users_profiles';
     protected $guarded = ['id'];
 
+    public static $rules = [
+        'name' => 'required|alpha|min:3',
+        'year' => 'required_with:year,month,date',
+        'month' => 'required_with:year,month,date',
+        'date' => 'required_with:year,month,date',
+
+        'profile_picture' => 'image|nullable|max:1999'
+    ];
+
+    public static $messages = [
+        'name.min' => 'Name must be at least 3 characters.',
+        'name.alpha' => 'Name must contain only characters.',
+        'profile_picture.image' => 'The file must be an image.',
+    ];
+
     public function user() {
         return $this->belongsTo('App\User', 'user_id');
     }
