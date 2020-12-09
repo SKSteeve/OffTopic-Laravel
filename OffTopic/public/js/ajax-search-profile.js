84c688f03444fa60ec2692e1cec6cff1d74d992f
SKSteeve <steeve1997.ss@gmail.com>
2020-12-09 23:59:19 +0200

    Added js for Api/SearchProfileController and profile_views.search-profile

---
+++ OffTopic/public/js/ajax-search-profile.js
@@ -0,0 +1,74 @@
+$(document).ready(function () {
+
+    var base_path = $("#url").val();
+
+    $("#name").keyup(function () {
+
+        let name = $("#name").val();
+
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+
+        $.ajax({
+            url: `${base_path}/api/search-profile`,
+            type: 'GET',
+            data: {
+                name: name
+            },
+            success: profiles,
+            error: errorReturned
+        });
+
+        function profiles(data) {
+            let users = data.data;
+
+
+            let table = $('.user-profiles .t-data');
+            table.empty();
+            for (let u = 0; u < users.length; u++) {
+                let user = users[u];
+                let tr = $('<tr>');
+
+                tr.attr('data-href', `${base_path}/users/profile/${user.id}`);
+                tr.on('click', function (e) {
+                    let tareget = e.target.parentNode;
+                    window.location.href = tareget.dataset.href;
+                });
+
+                for(let item in user) {
+                    console.log(user[item]);
+
+                    if(item === 'id') {
+                        continue;
+                    }
+
+                    let td = $('<td>');
+                    td.text(user[item]);
+                    tr.append(td);
+                }
+                table.append(tr);
+            }
+            //--------------------------------
+            return;
+            // for (let u = 0; u < users.length; u++) {
+            //
+            //
+            //
+            //         let td = $('<td>');
+            //         td.text(user[item]);
+            //         tr.append(td);
+            //     }
+            //     table.append(tr);
+            // }
+
+        }
+
+        function errorReturned(error) {
+            console.log(error);
+        }
+
+    });
+});
