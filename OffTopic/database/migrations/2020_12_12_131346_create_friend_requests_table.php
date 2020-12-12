60992cb7d353531a81980553ccbae4ae3614df24
SKSteeve <steeve1997.ss@gmail.com>
2020-12-12 19:14:48 +0200

    Created migration for table friend_requests

---
+++ OffTopic/database/migrations/2020_12_12_131346_create_friend_requests_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFriendRequestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('friend_requests', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('requested_user_id');
+            $table->foreign('requested_user_id')->references('id')->on('users');
+            $table->bigInteger('sender_user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('friend_requests');
+    }
+}
