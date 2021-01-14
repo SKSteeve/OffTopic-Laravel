f9108d3dcdaa7991157573a3da52e53dbcb08763
SKSteeve <steeve1997.ss@gmail.com>
2021-01-14 23:01:03 +0200

    Added deleteFriendRequest and checkIfFriendExist functions

--- OffTopic/app/Http/Controllers/FriendRequestsController.php
+++ OffTopic/app/Http/Controllers/FriendRequestsController.php
@@ -16,16 +16,30 @@ class FriendRequestsController extends Controller
      */
     public function store($id)
     {
-        $checkExistFriendRequest = FriendRequests::where('requested_user_id', $id)->where('sender_user_id', Auth::id())->first();
+        $checkFriendRequestFromSenderUser = FriendRequests::where('requested_user_id', $id)->where('sender_user_id', Auth::id())->first();
+        $checkFriendRequestFromRequestedUser = FriendRequests::where('requested_user_id', Auth::id())->where('sender_user_id', $id)->first();
 
-        if(Auth::id() != $id && $checkExistFriendRequest === null) {
-            $user = User::findOrFail($id);
+        if(Auth::id() != $id && $checkFriendRequestFromSenderUser === null) {
+
+            // check if their is existing request from the requested_user to the sender user ... if so they are automatically friends
+            if($checkFriendRequestFromRequestedUser) {
+                $friendShip = new FriendListController();
+                $friendShip->store(Auth::id(), $id);
+                $requestedUser = User::findOrFail($id);
+
+                return response()->json(['newFriend' => "You are now friends with {$requestedUser->name}"]);
+            }
 
             FriendRequests::create([
                 'requested_user_id' => $id,
                 'sender_user_id' => Auth::id(),
             ]);
-            return response()->json(['success' => "You sent friend request to {$user->name}"]);
+
+            $requestedUser = User::findOrFail($id);
+            $senderUser = User::findOrFail(Auth::id());
+
+            NotificationsController::createNotification('FriendRequest', 'Friend Request', "You received new friend request from {$senderUser->name}", $id, Auth::id());
+            return response()->json(['success' => "You sent friend request to {$requestedUser->name}"]);
         }
         return response()->json(['error' => "You can't send friend request to this user."]);
     }
@@ -33,7 +47,8 @@ class FriendRequestsController extends Controller
 
 
     /**
-     * Remove the specified resource from storage.
+     *  The sender Delete the friend request he sent ON RECEIVER PROFILE
+     *  The notification that receiver had is also deleted.
      *
      * @param  int  $id
      * @return \Illuminate\Http\Response
@@ -43,6 +58,39 @@ class FriendRequestsController extends Controller
         $friendRequest = FriendRequests::where('requested_user_id', $id)->where('sender_user_id', Auth::id())->first();
         $friendRequest->delete();
 
+        NotificationsController::deleteNotification('FriendRequest', 'Friend Request', $id, Auth::id());
+
         return response()->json(['success' => 'Removed friend request.']);
+    }
+
+
+    /**
+     *  The reciever/requested user, Delete friend request ON HIS NOTIFICATIONS
+     *
+     * @param $requestedUserId
+     * @param $senderUserId
+     */
+    public static function deleteFriendRequest($requestedUserId, $senderUserId)
+    {
+        $friendRequest = FriendRequests::where('requested_user_id', $requestedUserId)->where('sender_user_id', $senderUserId)->first();
+        $friendRequest->delete();
+    }
+
+
+    /**
+     *  Check if friend request exist
+     *
+     * @param $requestedUserId
+     * @param $senderUserId
+     * @return bool
+     */
+    public static function checkIfRequestExist($requestedUserId, $senderUserId)
+    {
+        $friendRequest = FriendRequests::where('requested_user_id', $requestedUserId)->where('sender_user_id', $senderUserId)->first();
+
+        if($friendRequest !== null) {
+            return true;
+        }
+        return false;
     }
 }
