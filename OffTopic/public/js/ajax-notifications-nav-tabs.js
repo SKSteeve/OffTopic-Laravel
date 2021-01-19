45ccf88753078080d7597bd686cae3c9d7692e39
SKSteeve <steeve1997.ss@gmail.com>
2021-01-19 22:47:45 +0200

    Created js file for notifications view -> nav tabs. The js response for changing the html after clicking on a nav tab

---
+++ OffTopic/public/js/ajax-notifications-nav-tabs.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+
+    var allTab = $('#all-tab');
+    var deletedTab = $('#deleted-tab');
+    var notDeletedTab = $('#not-deleted-tab');
+
+    var allNotificationsDataElement = $('#all');
+    var deletedNotificationsDataElement = $('#deleted');
+    var notDeletedNotificationsDataElement = $('#not-deleted');
+
+    var notificationsTabs = $('#notifications-tabs');
+
+    notificationsTabs.on('click', 'li', function (e) {
+
+        let clickedTab = e.target;
+        // console.log(clickedTab);
+
+        allTab.removeClass('active');
+        deletedTab.removeClass('active');
+        notDeletedTab.removeClass('active');
+
+        allNotificationsDataElement.removeClass('show active');
+        deletedNotificationsDataElement.removeClass('show active');
+        notDeletedNotificationsDataElement.removeClass('show active');
+
+        clickedTab.classList.add('active');
+        let clickedTabHref = clickedTab.getAttribute('href');
+
+        let tabNotificationsDataElementId = clickedTabHref.substring(1);
+        let tabNotificationsDataElement = $(`#${tabNotificationsDataElementId}`);
+
+        tabNotificationsDataElement.addClass('show active');
+        // console.log(tabNotificationsDataElement.text());
+    })
+});
