6e04666b0f8d30656c0c50bce58e016e8f07328d
SKSteeve <steeve1997.ss@gmail.com>
2021-01-27 23:29:07 +0200

    Created AboutMeController that returns the view about-me

---
+++ OffTopic/app/Http/Controllers/AboutMeController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class AboutMeController extends Controller
+{
+    public function index()
+    {
+        return view('about_views.about-me');
+    }
+}
